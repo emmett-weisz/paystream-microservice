d96607fb75af722a325ecf0e19068076c6446163
Emmett Weisz <emmettweisz@emmetts-mbp.mynetworksettings.com>
2025-06-11 14:45:06 -0400

    updated for  maintainability

--- examples/client.go
+++ examples/client.go
@@ -8,10 +8,14 @@ import (
 	pb "github.com/emmett-weisz/paystream-microservice/proto/paymentpb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
